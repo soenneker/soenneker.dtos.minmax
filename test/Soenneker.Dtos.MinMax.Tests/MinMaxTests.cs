f894178fd31dc5617ff4a812d8b70293693b0393
Jake Soenneker <jake@soeneker.com>
2024-12-22 13:58:56 +0000

    Tests

---
+++ test/Soenneker.Dtos.MinMax.Tests/MinMaxTests.cs
@@ -0,0 +1,18 @@
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Dtos.MinMax.Tests;
+
+[Collection("Collection")]
+public class MinMaxTests : FixturedUnitTest
+{
+    public MinMaxTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
