ebd7c294630cd1770e13f020c022dfdecef9a312
Jake Soenneker <jake@soenneker.com>
2025-03-26 20:29:16 -0500

    cleanup

--- src/MinMax.cs
+++ src/MinMax.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
@@ -9,17 +8,14 @@ namespace Soenneker.Dtos.MinMax;
 /// A minimal Record type with a Min (decimal) and a Max (decimal) and maximum JSON compatibility
 /// </summary>
 /// <remarks>Record type, avoid modification</remarks>
-[DataContract]
 public record MinMax
 {
     [Required]
-    [DataMember(Name = "min")]
     [JsonPropertyName("min")]
     [JsonProperty("min")]
     public decimal Min { get; set; }
 
     [Required]
-    [DataMember(Name = "max")]
     [JsonPropertyName("max")]
     [JsonProperty("max")]
     public decimal Max { get; set; }
